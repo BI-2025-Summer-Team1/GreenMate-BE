8f00a5c7937f796a1f17fb035eaeb685670ebe87
poketopa <lhs5427ll@gmail.com>
2025-07-08 22:53:28 +0900

    refactor: CommunityPost 엔티티의 user_id 컬럼을 User 객체 참조로 변경

--- src/main/java/kr/bi/greenmate/entity/CommunityPost.java
+++ src/main/java/kr/bi/greenmate/entity/CommunityPost.java
@@ -3,9 +3,12 @@ package kr.bi.greenmate.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -30,8 +33,9 @@ public class CommunityPost {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(length = 100, nullable = false)
     private String title;
