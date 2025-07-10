4e0f8e2b070cc051fec0d6bc04a49566724a1d3f
poketopa <lhs5427ll@gmail.com>
2025-07-10 18:56:27 +0900

    refactor: .gitignore 개행 추가 / 미사용 import 제거

--- src/main/java/kr/bi/greenmate/entity/CommunityPost.java
+++ src/main/java/kr/bi/greenmate/entity/CommunityPost.java
@@ -3,7 +3,6 @@ package kr.bi.greenmate.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,8 +17,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
