109e27cf0092c749ad70cdb26f172edf10b0ac08
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-07-28 11:31:17 +0900

    TEAM1-75 feat: 약관 엔티티 작성

---
+++ src/main/java/kr/bi/greenmate/entity/UserAgreement.java
@@ -0,0 +1,46 @@
+package kr.bi.greenmate.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserAgreement implements Serializable {
+
+    @Id
+    @Column(nullable = false)
+    private Long userId;
+
+    @Id
+    @Column(nullable = false)
+    private Long agreementId;
+
+    @Column(nullable = false)
+    private Integer isAccepted;
+
+    @Column
+    private LocalDateTime acceptedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "agreement_id", nullable = false, updatable = false)
+    private Agreement agreement;
+}
