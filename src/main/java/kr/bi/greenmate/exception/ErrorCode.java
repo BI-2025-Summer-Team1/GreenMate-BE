06158614694fbb3b7bdf5b8cc255755c1ad71b8d
poketopa <lhs5427ll@gmail.com>
2025-08-09 15:30:35 +0900

    refactor: 에러 코드 리팩토링

--- src/main/java/kr/bi/greenmate/exception/ErrorCode.java
+++ src/main/java/kr/bi/greenmate/exception/ErrorCode.java
@@ -6,19 +6,22 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    EMAIL_DUPLICATE("E01", "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
-    NICKNAME_DUPLICATE("E02", "이미 사용 중인 닉네임입니다.", HttpStatus.CONFLICT),
-    USER_NOT_FOUND("E03", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    FILE_EMPTY("E04", "업로드할 파일이 없습니다.", HttpStatus.BAD_REQUEST),
-    INVALID_IMAGE_TYPE("E05", "지원하지 않는 이미지 형식입니다.", HttpStatus.BAD_REQUEST),
-    MISSING_IMAGE_TYPE("E06", "이미지 타입이 누락되었습니다.", HttpStatus.BAD_REQUEST),
-    FILE_UPLOAD_FAIL("E07", "파일 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    SIGNUP_FAIL("E08", "회원가입 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    IMAGE_COUNT_EXCEED("E09", "이미지는 최대 10개까지 업로드 가능합니다.", HttpStatus.BAD_REQUEST),
-    IMAGE_SIZE_EXCEED("E10", "이미지 용량은 1MB를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
-    POST_NOT_FOUND("E11", "게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    CONCURRENT_LIKE_FAIL("E12", "동시성 문제로 좋아요 처리에 실패했습니다.", HttpStatus.CONFLICT),
-    RECRUITMENT_POST_NOT_FOUND("E21", "해당 ID의 모집 게시물을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    FILE_EMPTY("400-01", "업로드할 파일이 없습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_IMAGE_TYPE("400-02", "지원하지 않는 이미지 형식입니다.", HttpStatus.BAD_REQUEST),
+    MISSING_IMAGE_TYPE("400-03", "이미지 타입이 누락되었습니다.", HttpStatus.BAD_REQUEST),
+    IMAGE_COUNT_EXCEED("400-04", "이미지는 최대 10개까지 업로드 가능합니다.", HttpStatus.BAD_REQUEST),
+    IMAGE_SIZE_EXCEED("400-05", "이미지 용량은 1MB를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
+
+    USER_NOT_FOUND("404-01", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    POST_NOT_FOUND("404-02", "게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    RECRUITMENT_POST_NOT_FOUND("404-03", "해당 ID의 모집 게시물을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+    EMAIL_DUPLICATE("409-01", "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
+    NICKNAME_DUPLICATE("409-02", "이미 사용 중인 닉네임입니다.", HttpStatus.CONFLICT),
+    CONCURRENT_LIKE_FAIL("409-03", "동시성 문제로 좋아요 처리에 실패했습니다.", HttpStatus.CONFLICT),
+
+    FILE_UPLOAD_FAIL("500-01", "파일 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    SIGNUP_FAIL("500-02", "회원가입 처리 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
     private final String message;
