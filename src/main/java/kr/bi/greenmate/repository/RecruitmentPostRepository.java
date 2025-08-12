d13b9622d4c683a8fee5335e4bdac2ce11169a59
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-12 14:54:05 +0900

    TEAM1-28 refactor: EntityGraph로 RecruitmentPost 조회 쿼리 수정 (카테시안 곱 문제 해결)

--- src/main/java/kr/bi/greenmate/repository/RecruitmentPostRepository.java
+++ src/main/java/kr/bi/greenmate/repository/RecruitmentPostRepository.java
@@ -2,9 +2,8 @@ package kr.bi.greenmate.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import kr.bi.greenmate.entity.RecruitmentPost;
@@ -12,9 +11,6 @@ import kr.bi.greenmate.entity.RecruitmentPost;
 @Repository
 public interface RecruitmentPostRepository extends JpaRepository<RecruitmentPost, Long> {
 
-    @Query("SELECT DISTINCT rp FROM RecruitmentPost rp " +
-           "JOIN FETCH rp.user " +
-           "LEFT JOIN FETCH rp.images " +
-           "WHERE rp.id = :postId")
-    Optional<RecruitmentPost> findByIdWithUserAndImages(@Param("postId") Long postId);
+    @EntityGraph(attributePaths = {"user", "images"})
+    Optional<RecruitmentPost> findByIdWithUserAndImages(Long postId);
 }
