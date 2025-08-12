dd3383ad1ff24e5f4778fa0727f2b92c39ae4c15
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-12 15:22:59 +0900

    TEAM1-28 refactor: 카테시안 곱 문제 해결을 위해 쿼리 분리

--- src/main/java/kr/bi/greenmate/repository/RecruitmentPostRepository.java
+++ src/main/java/kr/bi/greenmate/repository/RecruitmentPostRepository.java
@@ -1,16 +1,22 @@
 package kr.bi.greenmate.repository;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import kr.bi.greenmate.entity.RecruitmentPost;
+import kr.bi.greenmate.entity.RecruitmentPostImage;
 
 @Repository
 public interface RecruitmentPostRepository extends JpaRepository<RecruitmentPost, Long> {
 
-    @EntityGraph(attributePaths = {"user", "images"})
-    Optional<RecruitmentPost> findByIdWithUserAndImages(Long postId);
+    @Query("SELECT rp FROM RecruitmentPost rp JOIN FETCH rp.user WHERE rp.id = :postId")
+    Optional<RecruitmentPost> findByIdWithUser(@Param("postId") Long postId);
+
+    @Query("SELECT rpi FROM RecruitmentPostImage rpi WHERE rpi.recruitmentPost.id = :postId")
+    List<RecruitmentPostImage> findImagesByPostId(@Param("postId") Long postId);
 }
