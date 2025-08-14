c9422210f7c144813f4f44d1603229ada3602450
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-14 21:28:14 +0900

    TEAM1-76 docs: 약관 조회 API Swagger 설명 수정

--- src/main/java/kr/bi/greenmate/controller/AgreementController.java
+++ src/main/java/kr/bi/greenmate/controller/AgreementController.java
@@ -20,7 +20,8 @@ public class AgreementController {
     private final AgreementService agreementService;
 
     @GetMapping
-    @Operation(summary = "전체 약관 조회", description = "회원가입 시 필요한 전체 약관 목록을 필수 및 선택 약관으로 구분하여 조회합니다.")
+    @Operation(summary = "전체 약관 조회", 
+    description = "회원가입 시 필요한 전체 약관 목록을 조회합니다. 필수 및 선택 약관이 하나의 리스트로 제공되며, 각 약관 객체의 isRequired 필드를 통해 구분할 수 있습니다.")
     public ResponseEntity<AgreementsResponse> getAllAgreements() {
         AgreementsResponse agreements = agreementService.getAllAgreements();
         return ResponseEntity.ok(agreements);
