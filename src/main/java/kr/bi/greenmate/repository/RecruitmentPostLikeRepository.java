89eee317f932b64f502f76eaedb8cec39c293600
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-19 16:54:06 +0900

    TEAM1-36 refactor: 불필요한 코드 제거

--- src/main/java/kr/bi/greenmate/repository/RecruitmentPostLikeRepository.java
+++ src/main/java/kr/bi/greenmate/repository/RecruitmentPostLikeRepository.java
@@ -3,9 +3,6 @@ package kr.bi.greenmate.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import kr.bi.greenmate.entity.RecruitmentPostLike;
 
@@ -14,10 +11,4 @@ public interface RecruitmentPostLikeRepository extends JpaRepository<Recruitment
     boolean existsByUser_IdAndRecruitmentPost_Id(Long userId, Long recruitmentPostId);
     
     Optional<RecruitmentPostLike> findByUser_IdAndRecruitmentPost_Id(Long userId, Long recruitmentPostId);
-
-    @Modifying
-    @Query("DELETE FROM RecruitmentPostLike l WHERE l.user.id = :userId AND l.recruitmentPost.id = :recruitmentPostId")
-    void deleteByUserAndRecruitmentPost(@Param("userId") Long userId, @Param("recruitmentPostId") Long recruitmentPostId);
-    
-    long countByRecruitmentPost_Id(Long recruitmentPostId);
 }
