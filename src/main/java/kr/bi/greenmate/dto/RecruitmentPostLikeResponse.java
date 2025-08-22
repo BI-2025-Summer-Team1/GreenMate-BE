61d239b1ef7378845b087323a89e2da0936274ec
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-22 10:33:46 +0900

    TEAM1-36 docs: Schema 어노테이션 추가

--- src/main/java/kr/bi/greenmate/dto/RecruitmentPostLikeResponse.java
+++ src/main/java/kr/bi/greenmate/dto/RecruitmentPostLikeResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "모집 게시물 좋아요 토글 및 좋아요 개수 응답 DTO")
 public class RecruitmentPostLikeResponse {
 
     @Schema(description = "좋아요 여부", example = "true")
