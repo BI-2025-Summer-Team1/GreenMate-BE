fcc5de766316d38753d02b95d1e7da0f37c05149
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-22 10:03:28 +0900

    TEAM1-36 fix: 불필요한 ArgsConstructor 코드들 삭제

--- src/main/java/kr/bi/greenmate/dto/RecruitmentPostLikeResponse.java
+++ src/main/java/kr/bi/greenmate/dto/RecruitmentPostLikeResponse.java
@@ -1,15 +1,11 @@
 package kr.bi.greenmate.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class RecruitmentPostLikeResponse {
 
     @Schema(description = "좋아요 여부", example = "true")
