2d26fde31e063b50883d1091605c0d3dd08f0751
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-25 11:36:41 +0900

    TEAM1-84 refactor: Spring의 트랜잭션 어노테이션으로 변경

--- src/main/java/kr/bi/greenmate/repository/RecruitmentPostCommentRepository.java
+++ src/main/java/kr/bi/greenmate/repository/RecruitmentPostCommentRepository.java
@@ -2,8 +2,8 @@ package kr.bi.greenmate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
 import kr.bi.greenmate.entity.RecruitmentPostComment;
 
 @Repository
