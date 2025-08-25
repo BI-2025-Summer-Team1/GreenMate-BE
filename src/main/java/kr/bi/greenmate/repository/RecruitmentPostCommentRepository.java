d59365a49e03595811e5ccfe77c60ff2cd8ed829
Yeojung <88540851+NostalgicJ@users.noreply.github.com>
2025-08-25 14:23:38 +0900

    TEAM1-43 refactor: BatchFetch 설정에 따라 불필요한 FETCH JOIN 제거

--- src/main/java/kr/bi/greenmate/repository/RecruitmentPostCommentRepository.java
+++ src/main/java/kr/bi/greenmate/repository/RecruitmentPostCommentRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import kr.bi.greenmate.entity.RecruitmentPostComment;
@@ -14,9 +12,7 @@ import kr.bi.greenmate.entity.RecruitmentPostComment;
 @Repository
 public interface RecruitmentPostCommentRepository extends JpaRepository<RecruitmentPostComment, Long> {
     
-    @Query("SELECT c FROM RecruitmentPostComment c JOIN FETCH c.user u WHERE c.recruitmentPost.id = :recruitmentPostId AND c.parentComment IS NULL")
-    Slice<RecruitmentPostComment> findByRecruitmentPostIdAndParentCommentIsNull(@Param("recruitmentPostId") Long recruitmentPostId, Pageable pageable);
+    Slice<RecruitmentPostComment> findByRecruitmentPostIdAndParentCommentIsNull(Long postId, Pageable pageable);
 
-    @Query("SELECT c FROM RecruitmentPostComment c JOIN FETCH c.user WHERE c.parentComment.id IN :parentIds")
-    List<RecruitmentPostComment> findByParentCommentIdIn(@Param("parentIds") List<Long> parentIds);
+    List<RecruitmentPostComment> findByParentCommentIdIn(List<Long> parentIds);
 }
