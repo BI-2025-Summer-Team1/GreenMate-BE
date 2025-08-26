7c34f842f5c0380e9a031babc0f0c125bd8eecf3
poketopa <lhs5427ll@gmail.com>
2025-08-26 20:53:46 +0900

    refactor: KeyserSliceResponse record로 변경

--- src/main/java/kr/bi/greenmate/dto/KeysetSliceResponse.java
+++ src/main/java/kr/bi/greenmate/dto/KeysetSliceResponse.java
@@ -3,22 +3,13 @@ package kr.bi.greenmate.dto;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-@Builder
 @Schema(description = "Keyset 기반 페이징 응답")
-public class KeysetSliceResponse<T> {
+public record KeysetSliceResponse<T>(
 
-	@Schema(description = "데이터 목록")
-	private final List<T> content;
+	@Schema(description = "데이터 목록") List<T> content,
 
-	@Schema(description = "다음 페이지 존재 여부")
-	private final Boolean hasNext;
+	@Schema(description = "다음 페이지 존재 여부") Boolean hasNext,
 
-	@Schema(description = "다음 페이지 커서 (마지막 아이템 id)")
-	private final Long lastId;
+	@Schema(description = "다음 페이지 커서 (마지막 아이템 id)") Long lastId) {
 }
